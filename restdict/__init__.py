7900a2babb5002247cdf71dd8e0a1e0d8782b3ee
gsergey99 <Sergio.Jimenezl@alu.uclm.es>
2020-11-03 01:11:41 +0100

    Fixing two errors remained

--- restdict/__init__.py
+++ restdict/__init__.py
@@ -17,8 +17,21 @@ def new_server(server_address):
     return DictServer(server_address)
 
 
-def new_restdict(server_api_uri):
+def new_restdict(server_api_uri, dict_name=None):
     '''
     Create new client connected to a given API URI
     '''
     return RestDict(server_api_uri)
+
+def delete_restdict(server_api_uri, dict_name=None):
+
+    
+    test_dict = RestDict(server_api_uri)
+
+    test_dict.__delitem__(dict_name)
+    
+    
+
+def connect_restdict(server_api_uri, dict_name=None):
+
+    return RestDict(server_api_uri)[dict_name]
